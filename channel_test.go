c28af7931e40c2d616ab5adf53e32a9e834d32fb
Vito Jihad Abdillah <jihadvito@gmail.com>
2025-09-14 22:34:12 +0700

    Channel as Parameter

--- channel_test.go
+++ channel_test.go
@@ -21,3 +21,20 @@ func TestCreateChannel(t *testing.T) {
 	fmt.Println(data)
 	time.Sleep(5 * time.Second)
 }
+
+func GiveMeResponse(channel chan string) { //pass by reference secara default
+	time.Sleep(2 * time.Second)
+	channel <- "Vito Jihad Abdillah"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+
+	fmt.Println(data)
+	time.Sleep(5 * time.Second)
+}
