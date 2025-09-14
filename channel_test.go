87c22ca4acf9d2775efc0fb8ff8a7aafcd6b19b8
Vito Jihad Abdillah <jihadvito@gmail.com>
2025-09-14 22:53:38 +0700

    BufferedChannel

--- channel_test.go
+++ channel_test.go
@@ -58,3 +58,22 @@ func TestInOutChannel(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Vito"
+		channel <- "Jihad"
+		channel <- "Abdillah"
+	}()
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("Done upload to buffer")
+}
