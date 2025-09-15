876b6661c8e10910ed91518fe83c29d8875319dd
Vito Jihad Abdillah <jihadvito@gmail.com>
2025-09-15 21:24:31 +0700

    Wait GrouP

---
+++ wait_group_test.go
@@ -0,0 +1,28 @@
+package go_goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func RunAsyncrhronous(group *sync.WaitGroup) {
+	defer group.Done()
+
+	group.Add(1)
+
+	fmt.Println("Hello")
+	time.Sleep(1 * time.Second)
+}
+
+func TestWaitGroup(t *testing.T) {
+	group := &sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go RunAsyncrhronous(group)
+	}
+
+	group.Wait()
+	fmt.Println("Complete")
+}
